45b7a357e05a70426be5b1398ec4e7ef3304654d
Ruben <rubenpenott@hotmail.com>
2024-03-16 18:26:20 -0400

    create added

--- blogs/models.py
+++ blogs/models.py
@@ -14,5 +14,5 @@ class Publication(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse ("publication_detail", kwargs={"pk": self.pk})
+        return reverse ("post_detail", kwargs={"pk": self.pk})
     
